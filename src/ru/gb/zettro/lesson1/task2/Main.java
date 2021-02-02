94d57bd0611535b0132f464c5038351aee48e3ad
Roman Russkikh <r-ru@yandex.ru>
2021-02-02 03:02:55 +0300

    Задание 2. Исправление ошибок и оптимизация кода

--- src/ru/gb/zettro/lesson1/task2/Main.java
+++ src/ru/gb/zettro/lesson1/task2/Main.java
@@ -1,10 +1,22 @@
 package ru.gb.zettro.lesson1.task2;
 
-// Из задания не совсем ясно чего мы должны достичь оптимизацией кода, без этого
-// понимания проблематично что-либо сделать. После исправления ошибок
-// код сам по себе вполне рабочий, как конкретно его нужно оптимизировать я не понял.
-// В зависимости от поставленной цели можно попробовать реализовать паттерны
-// Builder, Strategy, Abstract Factory.
+// Из задания не совсем ясно, чего мы должны достичь оптимизацией кода.
+// После исправления ошибок код сам по себе вполне рабочий, что касается
+// оптимизации, я сделал несколько поправок, опираясь на примеры из методички
+// (по принципам SOLID). Engine реализовал в виде абстрактного класса
+// (не стал его делать интерфейсом, поскольку хотел добавить параметр мощности
+// двигателя). Можно, конечно, еще было в интерфейс Movable добавить методы
+// start() и stop(), как это сделано в методичке, но мне показалось это
+// не совсем правильным с точки зрения принципа разделения интерфейсов.
+// Как вариант, можно было бы сделать один объединенный интерфейс
+// StartStoppable, в котором будут два метода start() и stop(),
+// добавить implements StartStoppable к абстрактному классу Car,
+// а потом сделать реализации этих методов в LightWeightCar и в Lorry.
+// Но тогда бы мы изменили функциональность класса LightWeightCar,
+// в котором изначально не было метода stop(). Из формулировки задачи
+// не понятно, умышленно это было сделано, или это тоже можно исправлять...
+// Если бы функциональность классов LightWeightCar и Lorry была одинаковая,
+// можно было бы еще сделать фабрику.
 //
 
 interface Movable {
@@ -15,26 +27,29 @@ interface Stoppable {
     void stop();
 }
 
-abstract class Car {
-    private Engine engine;
-    private String color;
+// сразу подключил интерфейс Movable к классу Car. Теперь все неабстрактные наследники Car
+// должны будут реализовать метод move()
+abstract class Car implements Movable {
     private String name;
+    private String color;
+    protected Engine engine;
 
-    //добавил конструктор
-    protected Car(Engine engine, String color, String name) {
+    // добавил конструктор
+    protected Car(Engine engine, String name, String color) {
         this.engine = engine;
-        this.color = color;
         this.name = name;
+        this.color = color;
     }
 
-    protected void start() {
+    // этот метод можно было бы включить в состав отдельного интерфейса и добавить implements
+    public void start() {
         engine.powerOn();
         System.out.println(name + " is starting");
     }
 
-    //убрал модификатор abstract, кому надо - переопределят
+    // убрал модификатор abstract, кому надо - переопределят
     public void open() {
-        System.out.println(name + " is opened");
+        System.out.println(name + " opened");
     }
 
     public Engine getEngine() {
@@ -64,64 +79,65 @@ abstract class Car {
     @Override
     public String toString() {
         return "Car{" +
-                "engine=" + engine +
+                "name='" + name + '\'' +
                 ", color='" + color + '\'' +
-                ", name='" + name + '\'' +
+                ", engine=" + engine +
                 '}';
     }
 }
 
-class LightWeightCar extends Car implements Movable {
+// Убрал Movable, так как абстрактный класс Car implements Movable.
+class LightWeightCar extends Car {
 
-    //добавил конструктор
-    public LightWeightCar(Engine engine, String color, String name) {
-        super(engine, color, name);
+    // добавил конструктор с двигателем внутреннего сгорания
+    public LightWeightCar(String name, String color) {
+        super(new CombustionEngine(), name, color);
     }
 
     // убрал @Override, поскольку здесь не идет речь о переопределении метода
     public void move() {
-        getEngine().getPower(10000);
+        engine.getPower(10000);
         System.out.println(getName() + " is moving");
     }
 
 }
 
-// вставил "implements" в нужной форме
-class Lorry extends Car implements Movable, Stoppable {
+// вставил implements, убрал Movable
+class Lorry extends Car implements Stoppable {
 
-    //добавил конструктор
-    public Lorry(Engine engine, String color, String name) {
-        super(engine, color, name);
+    // добавил конструктор с атомным двигателем
+    public Lorry(String name, String color) {
+        super(new AtomicEngine(), name, color);
     }
 
     public void move() {
-        getEngine().getPower(500000);
+        engine.getPower(500000);
         System.out.println(getName() + " is flying");
     }
 
     public void stop() {
-        System.out.println(getName() + " is landed");
-        getEngine().powerOff();
+        System.out.println(getName() + " landed");
+        engine.powerOff();
     }
 }
 
-//чтобы код компилировался добавил абстрактный класс Engine
+// чтобы код компилировался добавил абстрактный класс Engine
 // и два его наследника CombustionEngine и AtomicEngine
 abstract class Engine {
-    private int maxPower;
     private String name;
+    private int maxPower;
 
-    protected Engine(int maxPower, String name) {
-        this.maxPower = maxPower;
+    protected Engine(String name, int maxPower) {
         this.name = name;
+        this.maxPower = maxPower;
     }
 
     public void powerOn() {
-        System.out.println(name + " is powered on.");
+        System.out.println(name + " powered on.");
     }
 
     public void powerOff() {
-        System.out.println(name + " is powered off.");
+        System.out.println(name + " powered off.");
     }
 
     public int getPower(int desirableAmount) {
@@ -133,30 +149,29 @@ abstract class Engine {
     @Override
     public String toString() {
         return "Engine{" +
-                "maxPower=" + maxPower +
-                ", name='" + name + '\'' +
+                "name='" + name + '\'' +
+                ", maxPower=" + maxPower +
                 '}';
     }
 }
 
 class CombustionEngine extends Engine {
     public CombustionEngine() {
-        super(5000, "CombustionEngine");
+        super("CombustionEngine", 5000);
     }
 }
 
 class AtomicEngine extends Engine {
     public AtomicEngine() {
-        super(10000000, "AtomicEngine");
+        super("AtomicEngine", 10000000);
     }
 }
 
 
-
 class Main {
     public static void main(String[] args) {
-        LightWeightCar mini = new LightWeightCar(new CombustionEngine(), "Green", "Mini Countryman");
-        Lorry delorean = new Lorry(new AtomicEngine(), "Red", "DeLorean DMC-12");
+        LightWeightCar mini = new LightWeightCar("Mini Countryman", "Green");
+        Lorry delorean = new Lorry("DeLorean DMC-12", "Red");
 
         System.out.println(mini);
         System.out.println(delorean);
@@ -164,10 +179,10 @@ class Main {
         mini.open();
         mini.start();
         mini.move();
-        System.out.println();
+        System.out.println("Mini Countryman will never be stopped! =)) \n");
         delorean.open();
         delorean.start();
         delorean.move();
-        delorean.stop();
+        delorean.stop();  // у mini этого метода нет ;)
     }
 }
